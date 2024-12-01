e73682d0da9eac828abba053bb7ecddcc28d2a2c
Nickolas Kenyeres <nickolas@knicklabs.com>
2024-12-01 11:21:20 -0500

    Setup css modules

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -1,8 +1,13 @@
+import clsx from "clsx";
 import {
   Button as RACButton,
   type ButtonProps as RACButtonProps,
 } from "react-aria-components";
 
+import styles from "./button.module.css";
+
 export function Button(props: RACButtonProps) {
-  return <RACButton {...props} />;
+  return (
+    <RACButton {...props} className={clsx(styles.button, props.className)} />
+  );
 }
